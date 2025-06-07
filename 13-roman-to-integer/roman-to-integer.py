45fa90b1a13feaf7ff64890666a1d5afb2f9c381
Kush Patel <168948236+kushpatel1810@users.noreply.github.com>
2025-06-07 15:03:57 -0400

    Time: 11 ms (45.61%) | Memory: 12.4 MB (84.08%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.py
+++ 13-roman-to-integer/roman-to-integer.py
@@ -1,10 +1,6 @@
 class Solution(object):
     def romanToInt(self, s):
-        # priority_order = ['M','D','C','L','X','V','I']
         rank = {'M':1000, 'D':500, 'C':100, 'L':50, 'X':10, 'V':5, 'I':1}
-        # rank = {char:i for i, char in enumerate(priority_order)}
-        # print(rank)
-        # s = list(s)
         sum = 0
         i=0
         if len(s) == 1:
@@ -12,24 +8,13 @@ class Solution(object):
         while i < (len(s)-1):
             if rank[s[i]] >= rank[s[i+1]]:
                 sum+=rank[s[i]]
-                print(sum)
                 i+=1
                 if i == (len(s)-1):
                     sum+=rank[s[i]]
             else:
                 sum+=(rank[s[i+1]]-rank[s[i]])
-                print(sum)
                 i+=2
                 if i == (len(s)-1):
                     sum+=rank[s[i]]
                 
-        print(sum)
         return sum
-
-
-
-
-        #     print(s[0])
-        # print(rank)
-
-        
