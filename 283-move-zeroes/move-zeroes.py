831855c47749c97d98ec1d6fbcbf74ab8d1ae633
Kush Patel <168948236+kushpatel1810@users.noreply.github.com>
2025-09-27 18:51:33 -0400

    Time: 642 ms (6.05%) | Memory: 19 MB (31.51%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -1,10 +1,18 @@
+# class Solution:
+#     def moveZeroes(self, num: List[int]) -> None:
+#         for i in range(len(num)):
+#             if num[i]==0:
+#                 num[i]='A'
+#                 num.append(0)
+#         a='A'
+#         while a in num:
+#             num.remove(a)
+              
+
+
 class Solution:
     def moveZeroes(self, num: List[int]) -> None:
         for i in range(len(num)):
             if num[i]==0:
-                num[i]='A'
+                num.remove(num[i])
                 num.append(0)
-        a='A'
-        while a in num:
-            num.remove(a)
-              
